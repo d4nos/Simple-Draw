60a8f5fb3d88c7fd36b1a00ae5bd6700b15be08d
thoinvffa <83622767+thoinvffa@users.noreply.github.com>
2022-06-05 20:12:48 +0700

    update splash

--- app/src/main/kotlin/com/simplemobiletools/draw/pro/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/pro/activities/SplashActivity.kt
@@ -1,10 +1,19 @@
 package com.simplemobiletools.draw.pro.activities
 
 import android.content.Intent
+import android.os.Handler
 import com.simplemobiletools.commons.activities.BaseSplashActivity
+import com.simplemobiletools.draw.pro.R
 
 class SplashActivity : BaseSplashActivity() {
     override fun initActivity() {
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            openMain()
+        }, 2000)
+    }
+
+    fun openMain() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
